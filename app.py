eaaac82a54a7bf06048bb44e9e0865914537f2ac
okimoto-4117 <yokimoto@nikkotelecom.co.jp>
2026-09-10 17:28:44 +0900

    Update app.py

--- app.py
+++ app.py
@@ -284,7 +284,9 @@ def prepare_model_args(request_body, request_headers):
     model_args = {
         "messages": messages,
         "temperature": app_settings.azure_openai.temperature,
-        "max_tokens": app_settings.azure_openai.max_tokens,
+        #GPT-5.1対応2026/09/10
+        #"max_tokens": app_settings.azure_openai.max_tokens,
+        "max_completion_tokens": app_settings.azure_openai.max_completion_tokens,
         "top_p": app_settings.azure_openai.top_p,
         "stop": app_settings.azure_openai.stop_sequence,
         "stream": app_settings.azure_openai.stream,
@@ -1050,7 +1052,9 @@ async def generate_title(conversation_messages) -> str:
     try:
         azure_openai_client = await init_openai_client()
         response = await azure_openai_client.chat.completions.create(
-            model=app_settings.azure_openai.model, messages=messages, temperature=1, max_tokens=64
+            #GPT-5.1対応2026/09/10
+            #model=app_settings.azure_openai.model, messages=messages, temperature=1, max_tokens=64
+            model=app_settings.azure_openai.model, messages=messages, temperature=1, max_completion_tokens=64
         )
 
         title = response.choices[0].message.content
